e735460a81045a1cb15885ca60af36cc37c6b911
PanktiDoshi95 <pdd@narola.email>
2020-06-26 14:36:56 +0530

    Feat(): Code for displaying forecast grid from forecast upload json

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Handsontable from 'handsontable';
 
 @Component({
   selector: 'app-forecast',
@@ -6,10 +7,87 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forecast.component.scss']
 })
 export class ForecastComponent implements OnInit {
-
+  detailset = [];
+  columnObj = [];
+  forecastDateSet = [];
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.generateForecastGrid();
   }
 
+  generateForecastGrid() {
+    this.detailset = [
+      {
+        bucket: 'wk01',
+        forecast: '1'
+      },
+      {
+        bucket: 'wk02',
+        forecast: ''
+      },
+      {
+        bucket: 'wk03',
+        forecast: ''
+      },
+      {
+        bucket: 'wk04',
+        forecast: ''
+      },
+      {
+        bucket: 'wk05',
+        forecast: ''
+      },
+      {
+        bucket: 'wk06',
+        forecast: ''
+      },
+      {
+        bucket: 'wk07',
+        forecast: ''
+      },
+      {
+        bucket: 'wk08',
+        forecast: ''
+      }
+    ];
+
+    let rowObj: any = {};
+    this.detailset.forEach(element => {
+      const json = {};
+      json['title'] = element.bucket;
+      json['data'] = element.bucket;
+      this.columnObj.push(json)
+    });
+
+    this.detailset.forEach((element) => {
+      rowObj[element.bucket] = element.forecast
+      this.forecastDateSet.push(rowObj);
+    });
+
+    this.forecastDateSet = [...new Set(this.forecastDateSet)];
+
+    var example = document.getElementById('example');
+    var hot1 = new Handsontable(example, {
+      data: this.forecastDateSet,
+      colWidths: 100,
+      width: '100%',
+      height: 320,
+      rowHeights: 23,
+      rowHeaders: true,
+      colHeaders: true,
+      columns: this.columnObj,
+      afterChange: (change, source) => {
+        if (change !== null) {
+          let newValue = Number(change[0][3]);
+          let columnName = change[0][1];
+          this.detailset.forEach(element => {
+            if (element.bucket == columnName) {
+              element['forecast'] = newValue;
+            }
+          });
+        }
+      }
+    });
+  }
 }
