beeded73058f9ebc287770bccde90f443acabf0f
PanktiDoshi95 <pdd@narola.email>
2020-06-26 12:46:24 +0530

    Feat(): Installed handson library version -  7.4.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForecastComponent } from './forecast/forecast.component';
 import { PlanviewComponent } from './planview/planview.component';
 import { PlanviewAlternativeComponent } from './planview-alternative/planview-alternative.component';
+import { HotTableModule } from '@handsontable/angular';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { PlanviewAlternativeComponent } from './planview-alternative/planview-al
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HotTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
