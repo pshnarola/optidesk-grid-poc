b544238fb093f785856e5d721062e14cb86ff985
PanktiDoshi95 <pdd@narola.email>
2020-07-21 13:05:57 +0530

    Feat(): Created custom excel modal with jexcel

--- src/app/custom-excel/custom-excel.component.ts
+++ src/app/custom-excel/custom-excel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import * as jexcel from 'jexcel';
 
 @Component({
   selector: 'app-custom-excel',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomExcelComponent implements OnInit {
 
-  constructor() { }
+  private modalRef: NgbModalRef;
+  excelData = [
+    {
+      bucket: 'BUCKET',
+      forecast: 'FORECAST'
+    },
+    {
+      bucket: 'wk01',
+      forecast: ''
+    },
+    {
+      bucket: 'wk02',
+      forecast: ''
+    },
+    {
+      bucket: 'wk03',
+      forecast: ''
+    },
+    {
+      bucket: 'wk04',
+      forecast: ''
+    },
+    {
+      bucket: 'wk05',
+      forecast: ''
+    },
+    {
+      bucket: 'wk06',
+      forecast: ''
+    },
+    {
+      bucket: 'wk07',
+      forecast: ''
+    },
+    {
+      bucket: 'wk08',
+      forecast: ''
+    }
+  ];
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  openCustomExcel(excelContent) {
+    this.modalRef = this.modalService.open(excelContent);
+    this.createExcel();
+    this.modalRef.result.then((result) => {
+    }, (reason) => {
+    });
+  }
+
+  createExcel() {
+    jexcel(document.getElementById('excel'), {
+      data: this.excelData,
+      minDimensions: [30, 15],
+      columns: [
+        { width: 200 },
+        { width: 200 }
+      ],
+    });
   }
 
 }
