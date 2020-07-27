910df33b9cb6f0a3577daa7db1273ddccc20b1a5
PanktiDoshi95 <pdd@narola.email>
2020-07-27 15:36:59 +0530

    Feat(): Code for downloading excel and copy/paste data in excel modal

--- src/app/forecast-prototype/forecast-prototype.component.ts
+++ src/app/forecast-prototype/forecast-prototype.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared/services/shared.service';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import * as jexcel from 'jexcel';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-forecast-prototype',
@@ -12,10 +16,6 @@ export class ForecastPrototypeComponent implements OnInit {
   dataSet = {};
   chartData = [];
   forecastData = [
-    {
-      planBucket: 'PlanBucket',
-      forecast: 'Forecast'
-    },
     {
       planBucket: 'WK.01 2020',
       forecast: ''
@@ -99,8 +99,14 @@ export class ForecastPrototypeComponent implements OnInit {
   ];
   fileList = [];
   gridData = [];
+  errorMsg: any;
+  columnObj = [];
+
+  private modalRef: NgbModalRef;
+
   constructor(
-    private shared: SharedService
+    private shared: SharedService,
+    private modalService: NgbModal
   ) { }
 
   ngOnInit(): void {
@@ -119,13 +125,75 @@ export class ForecastPrototypeComponent implements OnInit {
       this.shared.uploadExcel(formData).then(res => {
         this.gridData = [];
         this.gridData = res;
-        this.gridData.unshift({
-          planBucket: 'PlanBucket',
-          forecast: 'Forecast'
-        });
       }).catch(error => {
       });
     } else {
     }
+  }
+
+  openExcel(excelContent) {
+    this.modalRef = this.modalService.open(excelContent);
+    this.createExcel();
+    this.modalRef.result.then((result) => {
+    }, (reason) => {
+    });
+  }
+
+  createExcel() {
+    this.columnObj = [
+      { width: 200, name: 'planBucket', title: 'PLANBUCKET', readOnly: false },
+      { width: 200, name: 'forecast', title: 'FORECAST', decimal: ',' }
+    ];
+    jexcel(document.getElementById('excel'), {
+      data: this.gridData,
+      columns: this.columnObj,
+      minDimensions: [30, 25],
+      rowResize: true,
+      columnDrag: true,
+      contextMenu: false,
+      onchange: (instance, cell, colIndex, rowIndex, value, oldValue) => {
+        const columnName = this.columnObj[colIndex].name;
+        if (Number(colIndex) === 1) {
+          if (this.gridData[rowIndex] && this.gridData[rowIndex].hasOwnProperty('planBucket')) {
+            this.gridData[rowIndex][columnName] = value;
+          } else {
+            if (value.length === 0) {
+              console.log('cell text', cell.text());
+              this.errorMsg = '';
+            } else {
+              this.errorMsg = 'Extra Data has been Added Only ' + this.gridData.length + 'Plan Buckets are available';
+            }
+          }
+        } else if (Number(colIndex) === 0) {
+          if (this.gridData[rowIndex] && this.gridData[rowIndex].hasOwnProperty('planBucket')) {
+            if (value !== this.gridData[rowIndex].planBucket) {
+              this.errorMsg = 'Plan bucket is not as per required formate';
+            } else {
+              this.errorMsg = '';
+              this.gridData[rowIndex][columnName] = value;
+            }
+          } else {
+            if (value.length === 0) {
+              this.errorMsg = '';
+            } else {
+              this.errorMsg = 'Extra Data has been Added Only ' + this.gridData.length + 'Plan Buckets are available';
+            }
+          }
+        }
+      },
+    });
+  }
+
+  downloadExcel() {
+    const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const EXCEL_EXTENSION = '.xlsx';
+    const fileName = 'Sample';
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.gridData);
+    const workbook: XLSX.WorkBook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const data: Blob = new Blob([excelBuffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + EXCEL_EXTENSION);
   }
 }
